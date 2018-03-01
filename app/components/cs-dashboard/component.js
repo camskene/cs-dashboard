04d862a4163fce3ab2345e77fa4096b933379559
Cam Skene <camskene@gmail.com>
2018-03-01 00:37:18 -0500

    Configure grid items width, height, and position based on gridstack.js array stub.
    Change style of demo boxes.

--- app/components/cs-dashboard/component.js
+++ app/components/cs-dashboard/component.js
@@ -1,48 +1,70 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import {
+  computed
+} from '@ember/object';
 
 export default Component.extend({
-  panels: computed(function() {
+  gridItems: computed(function() {
+    // example array from http://gridstackjs.com/demo/serialization.html
     return [{
-      description: 'Item one',
-      x1: 13,
-      x2: 1,
-      y1: 1,
-      y2: 3,
-    },{
-      description: 'Item two',
-      x1: 3,
-      x2: 1,
-      y1: 3,
-      y2: 5,
-    },{
-      description: 'Item three',
-      x1: 3,
-      x2: 1,
-      y1: 5,
-      y2: 13,
-    },{
-      description: 'Item four',
-      x1: 12,
-      x2: 13,
-      y1: 12,
-      y2: 13,
-    },{
-      description: 'Item five',
-    },{
-      description: 'Item six',
-    },{
-      description: 'Item seven',
-    },{
-      description: 'Item eight',
-    },{
-      description: 'Item nine',
-    },{
-      description: 'Item ten',
-    },{
-      description: 'Item eleven',
-    },{
-      description: 'Item twelve',
-    },];
+        "x": 2,
+        "y": 1,
+        "width": 2,
+        "height": 2
+      },
+      {
+        "x": 4,
+        "y": 0,
+        "width": 1,
+        "height": 1
+      },
+      {
+        "x": 0,
+        "y": 0,
+        "width": 2,
+        "height": 2
+      },
+      {
+        "x": 5,
+        "y": 3,
+        "width": 4,
+        "height": 2
+      },
+      {
+        "x": 3,
+        "y": 0,
+        "width": 1,
+        "height": 1
+      },
+      {
+        "x": 7,
+        "y": 0,
+        "width": 5,
+        "height": 3
+      },
+      {
+        "x": 1,
+        "y": 2,
+        "width": 1,
+        "height": 1
+      },
+      {
+        "x": 2,
+        "y": 0,
+        "width": 1,
+        "height": 1
+      }
+    ]
+  }),
+
+  panels: computed(function() {
+    return this.get('gridItems').map((item, index) => {
+      return Object.assign(item, {
+        "description": `Item ${index}`
+      }, {
+        "x": item.x + 1,
+        "y": item.y + 1,
+      });
+    });
   }),
 });
